897aaf3702b67aa5297e65ba5b5a5b8df4e42647
amorgadol <amorgado127@gmail.com>
2020-10-30 23:21:12 +0100

    Commit final

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,10 +4,12 @@ module.exports = {
   rules: {
     "selector-nested-pattern": "^&",
     indentation: 2,
+    "selector-max-specificity": "0,2,0",
     "no-descending-specificity": null,
     "no-eol-whitespace": null,
     "declaration-empty-line-before": null,
     "value-keyword-case": null,
+    "max-line-length": 80,
     "at-rule-no-unknown": [
       true,
       {
@@ -20,7 +22,6 @@ module.exports = {
           "use",
           "include",
           "mixin",
-          "extend",
         ],
       },
     ],
